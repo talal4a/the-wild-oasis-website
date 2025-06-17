43ee9f057fdd038cef2d634310e3a7e589337df7
talal4a <talalxus000@gmail.com>
2025-06-17 11:47:11 +0500

    update

---
+++ app/cabins/page.js
@@ -0,0 +1,8 @@
+import Navigation from "../components/Navigation";
+export default function Page() {
+  return (
+    <div>
+      <div> Cabin page</div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import Link from "next/link";
+import Navigation from "./components/Navigation";
 export default function Home() {
-  return <h1>Helo next</h1>;
+  return (
+    <div>
+      <h1>The Wild oasis.Welcome to paradise.</h1>
+      <Link href="/cabins">Explore luxary Cabins</Link>
+    </div>
+  );
 }
