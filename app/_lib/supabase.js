2ee2e53ad95335bbd325e712466b084ece3f2d46
talal4a <talalxus000@gmail.com>
2025-06-18 15:41:01 +0500

    update

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,5 +1,8 @@
+"use-client";
 import { createClient } from "@supabase/supabase-js";
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_KEY
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase environment variables are missing");
+}
+export const supabase = createClient(supabaseUrl, supabaseKey);
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rijxrhtrnqpkaqoargus.supabase.co",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
+};
 export default nextConfig;
