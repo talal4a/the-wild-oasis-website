dfbf8cc8c37aa0b00cc6f28d429084fefbebbb59
talal4a <talalxus000@gmail.com>
2025-06-20 22:31:04 +0500

    upadte

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -1,6 +1,5 @@
 import Navigation from '@/app/_components/Navigation';
 import Logo from '@/app/_components/Logo';
-
 function Header() {
   return (
     <header className='border-b border-primary-900 px-8 py-5'>
@@ -11,5 +10,4 @@ function Header() {
     </header>
   );
 }
-
 export default Header;
