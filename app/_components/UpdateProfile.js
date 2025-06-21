7223d999442a96702676d2ce66c530c513fa0b68
talal4a <talalxus000@gmail.com>
2025-06-21 18:50:21 +0500

    update

--- app/_components/UpdateProfile.js
+++ app/_components/UpdateProfile.js
@@ -35,7 +35,6 @@ export default function updateProfile() {
           defaultCountry={nationality}
         />
       </div>
-
       <div className="space-y-2">
         <label htmlFor="nationalID">National ID number</label>
         <input
@@ -43,7 +42,6 @@ export default function updateProfile() {
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
       </div>
-
       <div className="flex justify-end items-center gap-6">
         <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
           Update profile
