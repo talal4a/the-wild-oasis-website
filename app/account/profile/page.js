8234b1c5c54cbbd0602ff387ab0925127f277dc9
talal4a <talalxus000@gmail.com>
2025-06-22 18:19:19 +0500

    update

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -11,7 +11,6 @@ export default function Page() {
       <h2 className="font-semibold text-2xl text-accent-400 mb-4">
         Update your guest profile
       </h2>
-
       <p className="text-lg mb-8 text-primary-200">
         Providing the following information will make your check-in process
         faster and smoother. See you soon!
